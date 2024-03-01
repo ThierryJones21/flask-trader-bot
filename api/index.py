3171e14c441e3eed7f4df6a49f5ba13bf61b1b6e
Thierry Jones <jones.thierry@gmail.com>
2024-03-01 16:51:49 -0500

    added route

--- api/index.py
+++ api/index.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import gdown
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'I run google colabs!'
+
+@app.route('/run')
+def run_colab():
+    gdown.download('https://drive.google.com/drive/u/0/folders/1cCUx3v1OnZwvpjuYRbbyTyjEjBJlBzPQ', 
+                   'python-stock-predictor-alpaca-api.ipynb', quiet=False)
+    return jsonify(message='colab notebook ran successfully')
